0c4444acfcee1d5c6a8661bf2f57549784c6b0b3
Jieren Chen <jieren.chen@gmail.com>
2018-12-17 21:16:11 +0700

    fix

--- loss_function.py
+++ loss_function.py
@@ -1,9 +1,11 @@
 import torch
 
 # micro_fm
-def loss_function(y_pred, y_true):
-  beta = 1.0
-  beta2 = beta**2.0
-  top = torch.sum(y_true * y_pred)
-  bot = beta2 * torch.sum(y_true) + torch.sum(y_pred)
-  return -(1.0 + beta2) * top / bot
+def loss_function():
+  def ll(y_pred, y_true):
+    beta = 1.0
+    beta2 = beta**2.0
+    top = torch.sum(y_true * y_pred)
+    bot = beta2 * torch.sum(y_true) + torch.sum(y_pred)
+    return -(1.0 + beta2) * top / bot
+  return ll
